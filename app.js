cd29958e6d639756ff254560f0627807f0dcbede
Dahee Jo <happyj6528@gmail.com>
2022-01-05 14:57:29 +0900

    added HIDDEN_CLASS and greeting

--- app.js
+++ app.js
@@ -2,11 +2,16 @@
 // const loginInput = loginForm.querySelector("input");
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-
+const HIDDEN_CLASS = "hidden";
 function LoginSubmit(event) {
-    event.preventDefault();
-    console.log(loginInput.value);
+    event.preventDefault(); //submit 후 새로고침 막기
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASS);
+    console.log(username);
+    greeting.innerText = `Hello ${username}!`; //"Hello " + username +"!";
+    greeting.classList.remove(HIDDEN_CLASS);
     }
 
 loginForm.addEventListener("submit", LoginSubmit);
