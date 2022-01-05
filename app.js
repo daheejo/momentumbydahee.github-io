b07799f4cf624eb214a58a800c0098b3b3b33893
Dahee Jo <happyj6528@gmail.com>
2022-01-05 14:26:31 +0900

    added event.preventDefault()
    form을 submit하면 브라우저는 기본적으로 페이지를 새로고침 하도록 되어있다. preventDefault() 함수를 추가함으로써 브라우저의 기본 동작을 막을 수 있다!

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 // const loginForm = document.getElementById("login-form");
 // const loginInput = loginForm.querySelector("input");
-// const loginButton = loginForm.querySelector("button");
-
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function LoginBtnClick() {
-    const username = loginInput.value;
-    if (username ==="") {
-        alert("Write your name");
-    } else if (username.length > 15) {
-        alert("Name is too long");
+
+function LoginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value);
     }
-}
-loginButton.addEventListener("click", LoginBtnClick)
+
+loginForm.addEventListener("submit", LoginSubmit);
