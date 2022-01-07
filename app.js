9cdf64dee7531578deaa7c78e20fcd22624b08f8
Dahee Jo <happyj6528@gmail.com>
2022-01-07 17:02:42 +0900

    loading username

--- app.js
+++ app.js
@@ -5,13 +5,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASS = "hidden";
+const USERNAME_KEY = "username";
+ 
 function LoginSubmit(event) {
     event.preventDefault(); //submit 후 새로고침 막기
-    const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASS);
-    console.log(username);
-    greeting.innerText = `Hello ${username}!`; //"Hello " + username +"!";
-    greeting.classList.remove(HIDDEN_CLASS);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username)
+    showGreetings(username);
     }
 
-loginForm.addEventListener("submit", LoginSubmit);
+function showGreetings(username) {
+    greeting.innerText = `Hello ${username}!`; //"Hello " + username +"!";
+    greeting.classList.remove(HIDDEN_CLASS);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASS);
+    loginForm.addEventListener("submit", LoginSubmit);
+} else {
+    showGreetings(savedUsername);
+}
