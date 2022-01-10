27f53b105afc10b97735fa4b0d61c408596956b0
Dahee Jo <happyj6528@gmail.com>
2022-01-10 16:52:44 +0900

    saving todos

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,12 @@
 const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
+const toDos = [];
+
+function saveTodos() {
+    localStorage.setItem("todos",JSON.stringify(toDos)); // localstorage 데이터를 string화
+                                                        //왜냐하면 localstorage에선 문자열밖에 저장못하기 때문에
+}
 
 function deleteTodo(event) {
     const li = event.target.parentElement;
@@ -23,7 +29,9 @@ function handleTodoSubmit(event) {
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
+    toDos.push(newTodo);
     paintTodo(newTodo);
+    saveTodos();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
