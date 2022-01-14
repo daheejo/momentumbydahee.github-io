9a388d1def1a2cdc2ad3fb1493b80ecc0548729c
Dahee Jo <happyj6528@gmail.com>
2022-01-14 22:16:22 +0900

    weather api

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,17 @@
+const API_KEY = "fcedf70b69ecb9a333bfde01e55cea95";
+
 function GeoSuccess(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    console.log("you live in", lat, lng)
-}
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child")
+            const city = document.querySelector("#weather span:last-child")
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
 
 function GeoError() {
     alert("Can't find you.")
