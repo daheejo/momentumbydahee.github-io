3d027f443658c37c6e006cbe88d8fa47062c2194
Dahee Jo <happyj6528@gmail.com>
2022-01-18 21:31:59 +0900

    Update day7.js

--- challenge/day7.js
+++ challenge/day7.js
@@ -1,16 +1,18 @@
-const max = document.getElementById("max");
-const guessing = document.getElementById("guessing");
-const compare = document.querySelector("#result span fisrt-child");
-const result = document.querySelector("#result span last-child");
+const maxNum = document.getElementById("maxNum");
+const guessNum = document.getElementById("guessNum");
+const compare = document.querySelector("#result span:first-child");
+const result = document.querySelector("#result span:last-child");
+const gameForm = document.getElementById("game");
 
 function playGuess(event) {
   event.preventDefault();
-  const randomNum = Math.random()*max.length
-  compare.innerText = `You chose: ${guessing}, the machine chose: ${randomNum}.`
-  if (guessing === randomNum) {
-    result.innerText = "You won!"
+  const randomNum = Math.ceil(Math.random() * parseInt(maxNum.value));
+  compare.innerText = `You chose: ${guessNum.value}, the machine chose: ${randomNum}.`;
+  if (parseInt(guessNum.value) === randomNum) {
+    result.innerText = "You won!";
   } else {
-    result.innerText = "You lost!"
+    result.innerText = "You lost!";
   }
+}
 
-guessing.addEventListener("submit", playGuess);
+gameForm.addEventListener("submit", playGuess);
