0e4c62b9f7cb4a710d489d5b3f892976dd27d9fc
Dahee Jo <happyj6528@gmail.com>
2022-01-23 21:50:39 +0900

    after-login for hidden

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
-const afterLogin = document.getElementsByTagName("after-login");
+const afterLogin = document.getElementById("after-login")
 
 const HIDDEN_CLASS = "hidden";
 const USERNAME_KEY = "username";
@@ -12,24 +12,26 @@ function LoginSubmit(event) {
     event.preventDefault(); //submit 후 새로고침 막기
     loginForm.classList.add(HIDDEN_CLASS);
     const usernameTyped = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, usernameTyped) //local storage 저장
+    localStorage.setItem(USERNAME_KEY, usernameTyped); //local storage 저장
     showGreetings(usernameTyped);
 }
+
 function showGreetings(username) {
     // const username = localStorage.getItem(USERNAME_KEY);
+    afterLogin.classList.remove(HIDDEN_CLASS);
     greeting.innerText = `Hello ${username}!`; //"Hello " + username +"!";
     greeting.classList.remove(HIDDEN_CLASS);
-    // afterLogin.classList.remove(HIDDEN_CLASS);
 }
 
 function loadName() {
-const savedUsername = localStorage.getItem(USERNAME_KEY);
-if (savedUsername === null) {
-    loginForm.classList.remove(HIDDEN_CLASS);
-    loginForm.addEventListener("submit", LoginSubmit);
-} else {
-    showGreetings(savedUsername);
-}}
+    const savedUsername = localStorage.getItem(USERNAME_KEY);
+    if (savedUsername === null) {
+        loginForm.classList.remove(HIDDEN_CLASS);
+        loginForm.addEventListener("submit", LoginSubmit);
+    } else {
+        showGreetings(savedUsername);
+    }
+}
 
 function init() {
     loadName();
