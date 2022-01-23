f5ca99acc1e03a180b82024755da3d98c3246a42
Dahee Jo <happyj6528@gmail.com>
2022-01-24 01:20:15 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const API_KEY = "fcedf70b69ecb9a333bfde01e55cea95";
 function GeoSuccess(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
     fetch(url)
         .then(response => response.json())
         .then((data) => {
