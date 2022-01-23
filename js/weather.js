011b99b14d6de119e7ab289f65c1ac5c7835b0aa
Dahee Jo <happyj6528@gmail.com>
2022-01-24 00:48:57 +0900

    arranged weather

--- js/weather.js
+++ js/weather.js
@@ -9,8 +9,8 @@ function GeoSuccess(position) {
         .then((data) => {
             const weather = document.querySelector("#weather span:first-child")
             const city = document.querySelector("#weather span:last-child")
-            city.innerText = data.name;
-            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+            city.innerText = `  @${data.name}`;
+            weather.innerText = `${data.weather[0].main} / ${Math.floor(data.main.temp)}°`;
         });
     }
 function GeoError() {
